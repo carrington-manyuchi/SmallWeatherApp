8bca0fb85fc066d3a4b4cc93c3210fb15bfe7d30
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-11 20:11:21 +0200

    Initial Commit

---
+++ SmallWeatherApp/SmallWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallWeatherAppApp.swift
+//  SmallWeatherApp
+//
+//  Created by Manyuchi, Carrington C on 2025/05/11.
+//
+
+import SwiftUI
+
+@main
+struct SmallWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
